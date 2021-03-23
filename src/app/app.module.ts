62365abd285716b6f84289dfb4aa97435ec86729
Ricardo <ricardofariasxyz@gmail.com>
2021-03-23 14:12:38 -0300

    toggle sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
