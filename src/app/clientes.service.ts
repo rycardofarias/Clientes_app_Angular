10bfad1f9f8f9ec624f145f4011d8973dfa833a1
Ricardo <ricardofariasxyz@gmail.com>
2021-03-27 00:30:03 -0300

    Save client integration Api Rest

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -2,6 +2,7 @@ import { Cliente } from './clientes/cliente';
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ import { HttpClient} from '@angular/common/http'
 export class ClientesService {
 
   constructor(private http:HttpClient) { }
+
+  salvar (cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
 
   getCliente(): Cliente{
     let cliente = new Cliente();
