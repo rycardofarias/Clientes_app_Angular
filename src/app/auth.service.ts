a9e131193b1d7d2b88b28def964bafc31b92a776
Ricardo <ricardofariasxyz@gmail.com>
2021-04-25 02:18:41 -0300

    Finalizing the User registration

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,20 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
+import { Usuario } from './login/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrlBase: string = environment.apiUrlBase+"/api/usuarios"
+  
+  constructor(private http: HttpClient) {
+
+   }
+   salvar(usuario: Usuario) : Observable<any>{
+     return this.http.post<any>(this.apiUrlBase, usuario);
+   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { AuthService } from './../auth.service';
+import { Usuario } from './usuario';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,10 +12,15 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean;
-  
-  constructor(private router: Router) { }
+  mensagemSucesso: string;
+  erros: String[];
+
+
+  constructor(
+    private router: Router,
+    private authService : AuthService
+    ) { }
 
   ngOnInit(): void {
   }
@@ -26,5 +34,18 @@ export class LoginComponent implements OnInit {
   }
   cancelaCadastro(){
     this.cadastrando=false;
+  }
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService
+          .salvar(usuario)
+          .subscribe( response => {
+            this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o Login"
+          },errorResponse => {
+            this.mensagemSucesso=null;
+            this.erros = errorResponse.error.erros;
+          })
   }
 }
